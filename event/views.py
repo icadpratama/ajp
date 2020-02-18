e6d1e8406b69de4959d882288f48846de18b8752
Irsyad Jamal Pratama Putra <irsyadjpratamap@gmail.com>
2020-02-18 17:41:10 +0700

    Add pagination to Events page

--- event/views.py
+++ event/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .models import Event
 
 # Create your views here.
 def list(request):
-    qs = Event.objects.all().published() # queryset -> list of python object
-    if request.user.is_authenticated:
-        my_qs = Event.objects.filter(user=request.user)
-        qs = (qs | my_qs).distinct()
+    qs_list = Event.objects.all().published() # queryset -> list of python object
+    paginator = Paginator(qs_list, 2)
+    page_number = request.GET.get('page')
+    qs = paginator.get_page(page_number)
     template_name = 'event.html'
     context = {'object_list': qs}
     return render(request, template_name, context)
